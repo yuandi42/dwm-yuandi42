cc71efdc4b772f841206b0d5e77f2c01a2af72f8
yuandi42 <realyuandi42@gmail.com>
2022-03-22 00:55:53 +0800

    change colorscheme

--- config.def.h
+++ config.def.h
@@ -5,17 +5,17 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "monospace:size=14" };
+static const char dmenufont[]       = "monospace:size=14";
+static const char col_gray1[]       = "#1d2021";
+static const char col_gray2[]       = "#98971a";
+static const char col_gray3[]       = "#a89984";
+static const char col_gray4[]       = "#b8bb26";
+static const char col_cyan[]        = "#ebdbb2";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray1, col_gray4,  col_gray4  },
 };
 
 /* tagging */
