9201d82ca28bbf31482606870fecd9ea68adb2a6
yuandi42 <realyuandi42@gmail.com>
2022-03-22 01:10:14 +0800

    change border colors

--- config.def.h
+++ config.def.h
@@ -14,7 +14,7 @@ static const char col_gray4[]       = "#b8bb26";
 static const char col_cyan[]        = "#ebdbb2";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
 	[SchemeSel]  = { col_gray1, col_gray4,  col_gray4  },
 };
 
