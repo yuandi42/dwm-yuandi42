c42c92912befac1c455f4eb842e8aeb4135a622a
yuandi42 <realyuandi42@gmail.com>
2022-03-22 21:03:43 +0800

    add vanitygaps.c

--- config.def.h
+++ config.def.h
@@ -8,6 +8,11 @@ static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corne
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
 static const char *fonts[]          = { "UbuntuMono Nerd Font:size=14" };
@@ -42,11 +47,26 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+#include "vanitygaps.c"
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "[@]",      spiral },
+	{ "[\\]",     dwindle },
+	{ "H[]",      deck },
+	{ "TTT",      bstack },
+	{ "===",      bstackhoriz },
+	{ "HHH",      grid },
+	{ "###",      nrowgrid },
+	{ "---",      horizgrid },
+	{ ":::",      gaplessgrid },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ NULL,       NULL },
 };
 
 /* key definitions */
@@ -68,8 +88,6 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	// { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	// { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ALTKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -78,10 +96,26 @@ static Key keys[] = {
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	// { MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ALTKEY,                XK_u,      incrgaps,       {.i = +1 } },
+	{ MODKEY|ALTKEY|ShiftMask,      XK_u,      incrgaps,       {.i = -1 } },
+	{ MODKEY|ALTKEY,                XK_i,      incrigaps,      {.i = +1 } },
+	{ MODKEY|ALTKEY|ShiftMask,      XK_i,      incrigaps,      {.i = -1 } },
+	{ MODKEY|ALTKEY,                XK_o,      incrogaps,      {.i = +1 } },
+	{ MODKEY|ALTKEY|ShiftMask,      XK_o,      incrogaps,      {.i = -1 } },
+	{ MODKEY|ALTKEY,                XK_6,      incrihgaps,     {.i = +1 } },
+	{ MODKEY|ALTKEY|ShiftMask,      XK_6,      incrihgaps,     {.i = -1 } },
+	{ MODKEY|ALTKEY,                XK_7,      incrivgaps,     {.i = +1 } },
+	{ MODKEY|ALTKEY|ShiftMask,      XK_7,      incrivgaps,     {.i = -1 } },
+	{ MODKEY|ALTKEY,                XK_8,      incrohgaps,     {.i = +1 } },
+	{ MODKEY|ALTKEY|ShiftMask,      XK_8,      incrohgaps,     {.i = -1 } },
+	{ MODKEY|ALTKEY,                XK_9,      incrovgaps,     {.i = +1 } },
+	{ MODKEY|ALTKEY|ShiftMask,      XK_9,      incrovgaps,     {.i = -1 } },
+	{ MODKEY|ALTKEY,                XK_0,      togglegaps,     {0} },
+	{ MODKEY|ALTKEY|ShiftMask,      XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	// { MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
@@ -100,7 +134,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */
